1c809f4b08a07748f941157be8f8cb5db22f14ea
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-06 21:21:38 -0500

    forecast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from env import *
+from datetime import datetime
 
 key = os.environ.get('WEATHER_KEY')
 url = os.environ.get('BASE_URL')
@@ -8,14 +9,22 @@ url = os.environ.get('BASE_URL')
 def main():
     city = input('Please enter a city:\n').lower().strip()
     country = input('Please enter a country code:\n').lower().strip()
-    query_type = 'weather'
+    query_type = 'forecast'
 
     data = api_call(city, country, query_type)
-    print(data)
-    if data:
+    if data and query_type == 'weather':
         weather_description = data['weather'][0]['description']
         temp = data['main']['temp']
         print(f'The weather is {weather_description}, the temperature is {temp:.2f}F.')
+    elif data and query_type == 'forecast':
+        forecast_items = data['list']
+        for forecast in forecast_items:
+            timestamp = forecast['dt']
+            date = datetime.fromtimestamp(timestamp)
+            temp = forecast['main']['temp']
+            wind = forecast['wind']['speed']
+            print(f'at {date} temp is {temp}F, and wind is {wind}')
+        print(data)
 
 
 def api_call(city, country, query_type):
@@ -24,9 +33,8 @@ def api_call(city, country, query_type):
         try:
             query = {'q': f'{city},{country}', 'units': 'imperial', 'appid': key}
             resp = requests.get(f'{url}{query_type}', params=query).json()
-            print(resp)
             response_code = resp['cod']
-            if response_code == 200:
+            if int(response_code) == 200:
                 return_resp = resp
             else:
                 raise Exception(f'Bad response: {response_code}')
@@ -41,24 +49,6 @@ if __name__ == '__main__':
     main()
 
 
-
-
-
-
-#"http://api.openweathermap.org/data/2.5/weather?q=paris,fr&units=imperial&appid=6b9aabd5fe36a23f54cc26c372236a65"
-# Part 1: Weather Forecast
-#
-# Use the forecast API to create a detailed, neatly formatted 5-day forecast, for anywhere the user chooses.
-#
-# Make sure your your API key is not coded into your program. It should read the key from an environment variable.
-#
-# Use a query parameter dictionary in the request.
-#
-# Your forecast should show the temperature and unit (F or C), weather description, and wind speed for every three hour interval, over the next 5 days.
-#
-# Your program should handle errors. What type of errors do you anticipate? How will you deal with them?
-#
-# Part 2: Time choice
 #
 # Will you show the local time in Minnesota, or the UTC time? Why? Add some comments to your program explaining your choice. Reading: Unix Time
 #
